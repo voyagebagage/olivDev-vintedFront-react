dc0c20d775ce79d5914a075f67e3cde70742d3e5
Olivier Frugier <sedatif2@MacBook-Pro-de-Olivier.local>
2021-05-08 00:03:58 +0700

    finished home soon

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -1,11 +1,87 @@
+import "./Home.css";
 import { Link } from "react-router-dom";
+import { useEffect, useState } from "react";
+import axios from "axios";
 
-const Home = () => {
-  const id = 4323424;
-  return (
-    <div className="container">
-      <Link to={`/offer/${id}`}>Offer</Link>
+const Offer = () => {
+  const [data, setData] = useState({});
+  const [isLoading, setIsLoading] = useState(true);
+  useEffect(() => {
+    const fetchData = async () => {
+      const response = await axios.get(
+        "https://lereacteur-vinted-api.herokuapp.com/offers"
+      );
+      setData(response.data);
+      setIsLoading(false);
+    };
+    fetchData();
+  }, []);
+  return isLoading ? (
+    <div className="downloading">
+      <span>-_D_O_W_N_L_O_A_D_I_N_G_-</span>
+      {/* <div className="downloading"></div> */}
+      <div
+        style={{
+          width: "100%",
+          height: "0",
+          paddingBottom: "68%",
+          position: "relative",
+        }}
+      >
+        <img
+          src="https://media.giphy.com/media/curuTbwjOnsRO/giphy.gif"
+          width="100%"
+          height="100%"
+          style={{ position: "absolute" }}
+          frameBorder="0"
+          allowFullScreen
+          alt={"DL"}
+        />
+      </div>
     </div>
+  ) : (
+    <>
+      <Link to="/">Home</Link>
+      <section className="header-top">
+        <div className="full-length-top-photo">
+          <div className="whiteBox"></div>
+        </div>
+      </section>
+      <section className="offers-display">
+        {data.offers.map((offer) => {
+          return (
+            <p
+              className="card-container"
+              key={offer._id}
+              style={{ border: "2px solid aqua" }}
+            >
+              {/* {console.log(offer.owner.account.avatar.secure_url)}; */}
+              {/* {console.log(offer.product_details[0])} */}
+              <div className="card-avatar-username">
+                <img src={offer.owner.account.avatar.secure_url} alt={"B"} />
+                <h2>{offer.owner.account.username}</h2>
+                <img
+                  src={offer.product_image.secure_url}
+                  alt={offer.product_description}
+                />
+              </div>
+              <div>
+                <span>{offer.product_price}</span>
+                {offer.product_details.map((productDetail) => {
+                  {
+                    console.log(productDetail.MARQUE);
+                  }
+                  return <span>{productDetail.MARQUE}</span>;
+                })}
+
+                <span></span>
+              </div>
+            </p>
+          );
+        })}
+      </section>
+    </>
   );
 };
-export default Home;
+
+export default Offer;
--- src/containers/Offer.js
+++ src/containers/Offer.js
@@ -1,18 +1,53 @@
-import "./Offer.css";
+// import "./Offer.css";
 // import { Link } from "react-router-dom";
-import { Link } from "react-router-dom";
+// import { useEffect, useState } from "react";
+// import axios from "axios";
 
-const Offer = () => {
-  return (
-    <div>
-      <Link to="/">Home</Link>
-      <section>
-        <div className="full-length-top-photo">
-          <div className="whiteBox"></div>
-        </div>
-      </section>
-    </div>
-  );
-};
+// const Offer = () => {
+//   const [data, setData] = useState({});
+//   const [isLoading, setIsLoading] = useState(true);
+//   useEffect(() => {
+//     const fetchData = async () => {
+//       const response = await axios.get(
+//         "https://lereacteur-vinted-api.herokuapp.com/offer/:id"
+//       );
+//       setData(response.data);
+//       setIsLoading(false);
+//     };
+//     fetchData();
+//   }, []);
+//   return
+// isLoading ? (
+//   <div className="downloading">
+//     <span>-_D_O_W_N_L_O_A_D_I_N_G_-</span>
+//     {/* <div className="downloading"></div> */}
+//     <div
+//       style={{
+//         width: "100%",
+//         height: "0",
+//         paddingBottom: "68%",
+//         position: "relative",
+//       }}
+//     >
+//       <img
+//         src="https://media.giphy.com/media/curuTbwjOnsRO/giphy.gif"
+//         width="100%"
+//         height="100%"
+//         style={{ position: "absolute" }}
+//         frameBorder="0"
+//         class="giphy-embed"
+//         allowFullScreen
+//       ></img>
+//     </div>
+//     <p>
+//       <a href="https://giphy.com/gifs/mrdiv-curuTbwjOnsRO"></a>
+//     </p>
+//   </div>
+// ) : (
+// <div>
+//   <Link to="/">Home</Link>
+// </div>
+// );
+// };
 
-export default Offer;
+// export default Offer;
